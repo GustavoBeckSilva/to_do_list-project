9720e0e0da07ff023c5dbadc07f02c145fbcb180
GustavoBeckSilva <gustavogbks@gmail.com>
2025-06-03 23:49:59 -0300

    feat(backend/setup): inicializar projeto com Sequelize, PostgreSQL e modelo/migração Tarefa

---
+++ backend/migrations/20250604022526-create-tarefa.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tarefas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      concluida: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      prazo: {
+        type: Sequelize.DATEONLY,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tarefas');
+  }
+};
---
+++ backend/models/tarefa.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tarefa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tarefa.init({
+    descricao: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    concluida: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    prazo: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Tarefa',
+  });
+  return Tarefa;
+};
